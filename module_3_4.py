1dac7163c9bcc513bd8a1324ab50f73642aae031
Nikiliks <nktalpeev@gmail.com>
2024-08-29 06:15:06 +0300

    Update module_3_4.py

--- module_3_4.py
+++ module_3_4.py
@@ -1,9 +1,10 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for i in other_words:
-        if root_word.lower() in i.lower():
+        if root_word.lower() in i.lower() or i.lower() in root_word.lower():
             same_words.append(i)
     return same_words
+
 result = single_root_words ('sold', 'solder', 'holder', 'chery', 'Barasoldary')
 print(result)
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
